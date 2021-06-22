86be4ada1f8ea628f374a8301f1d575e41b1b1c8
Qudix <17361645+Qudix@users.noreply.github.com>
2021-06-22 00:25:15 -0500

    Update

--- etc/cmake/sourcelist.cmake
+++ etc/cmake/sourcelist.cmake
@@ -1,5 +1,13 @@
-set(sources ${sources}
-	src/main.cpp
-	src/PCH.cpp
+set(SOURCES
+	src/PCH.h
+	src/Papyrus/Papyrus.h
+	src/Papyrus/PapyrusCamera.h
+	src/Papyrus/PapyrusDatabase.h
+	src/Papyrus/PapyrusFace.h
+	src/Papyrus/PapyrusObject.h
+	src/Papyrus/PapyrusUtil.h
+	src/Patch.h
 	src/Util.cpp
+	src/Util.h
+	src/main.cpp
 )
--- src/PCH.h
+++ src/PCH.h
@@ -16,40 +16,6 @@ namespace WinAPI
 namespace fs = std::filesystem;
 using json = nlohmann::json;
 
-#ifndef NDEBUG
-#include <spdlog/sinks/base_sink.h>
-
-namespace logger
-{
-	template <class Mutex>
-	class msvc_sink :
-		public spdlog::sinks::base_sink<Mutex>
-	{
-	private:
-		using super = spdlog::sinks::base_sink<Mutex>;
-
-	public:
-		explicit msvc_sink() {}
-
-	protected:
-		void sink_it_(const spdlog::details::log_msg& a_msg) override
-		{
-			spdlog::memory_buf_t formatted;
-			super::formatter_->format(a_msg, formatted);
-			WinAPI::OutputDebugString(fmt::to_string(formatted).c_str());
-		}
-
-		void flush_() override {}
-	};
-
-	using msvc_sink_mt = msvc_sink<std::mutex>;
-	using msvc_sink_st = msvc_sink<spdlog::details::null_mutex>;
-
-	using windebug_sink_mt = msvc_sink_mt;
-	using windebug_sink_st = msvc_sink_st;
-}
-#endif
-
 using namespace std::literals;
 
 namespace logger
