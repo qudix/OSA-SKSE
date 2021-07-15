d046578e2874f9b3a91ed2116af59a44039724bd
Sairion350 <76807368+Sairion350@users.noreply.github.com>
2021-07-15 00:41:06 -0500

    Many new functions

--- src/Papyrus/PapyrusUtil.h
+++ src/Papyrus/PapyrusUtil.h
@@ -1,28 +1,72 @@
 #pragma once
+#include <random>
+
 
 namespace PapyrusUtil
 {
     using VM = RE::BSScript::IVirtualMachine;
 
-	// Sairion, what the heck does this do?
-	int32_t GetFormID(RE::StaticFunctionTag*, RE::TESForm* a_form)
+	
+	RE::TESForm* NewObject(RE::StaticFunctionTag*, std::string classtype)
 	{
-		if (!a_form)
-			return -1;
+		logger::info("{} a", classtype);
 
-		auto d = a_form->formID;
-		auto c = a_form->formID;
-		auto act = a_form->As<RE::Actor>();
-		auto h = act->GetActorBase()->formID;
+		auto const formType = RE::FormType::Quest;
+		auto factory = RE::IFormFactory::GetFormFactoryByType(formType);
+		auto form = factory->Create();
 
-		return d + c + h;
+
+		auto virtualmachine = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+
+		auto handle = virtualmachine->GetObjectHandlePolicy1()->GetHandleForObject(formType, form);
+		RE::BSTSmartPointer<RE::BSScript::Object> script;
+
+		virtualmachine->CreateObject2(classtype, script);
+
+
+		virtualmachine->BindObject(script, handle, false);
+		
+		return form;
+	}
+
+	void DeleteObject(RE::StaticFunctionTag*, RE::TESForm* form)
+	{
+		//form->SetDelete(true);
+		auto const formType = RE::FormType::Quest;
+		auto virtualmachine = RE::BSScript::Internal::VirtualMachine::GetSingleton();
+
+		auto handle = virtualmachine->GetObjectHandlePolicy1()->GetHandleForObject(formType, form);
+
+		virtualmachine->ResetAllBoundObjects(handle);
+		virtualmachine->GetObjectBindPolicy()->bindInterface->RemoveAllBoundObjects(handle);
+		// dangerous??
+		delete(form);
+	}
+
+	int RandomInt(RE::StaticFunctionTag*, int min, int max)
+	{
+		std::random_device rd;
+    	std::uniform_int_distribution<int> dist(min, max);
+   		std::mt19937 mt(rd());
+
+   		return dist(mt);
+	}
+
+	float RandomFloat(RE::StaticFunctionTag*, float min, float max)
+	{
+		return ((max - min) * ((float)rand() / RAND_MAX)) + min;
 	}
 
 	bool Bind(VM* a_vm)
 	{
 		const auto obj = "OSANative"sv;
 
-		BIND(GetFormID);
+		BIND(NewObject);
+		BIND(DeleteObject);
+		BIND(RandomInt, true);
+		BIND(RandomFloat, true);
+
+
 
 		return true;
 	}
