d3adcde5ed645b0856fb4a5423b1cb6f99f2b111
Qudix <17361645+Qudix@users.noreply.github.com>
2021-09-02 17:43:33 -0500

    Update project

--- etc/bootstrap.ps1
+++ etc/bootstrap.ps1
@@ -1,5 +1,4 @@
 Set-Location ".."
-git submodule update --init --recursive
 cmake -B "build" -S "."
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
